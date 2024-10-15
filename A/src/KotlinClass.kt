ba827f385ad0f65e256d4b1859b2798cc203bb2b
Lev Leontev <lev.leontev@jetbrains.com>
2024-10-15 10:11:07 -0700

    initial

---
+++ A/src/KotlinClass.kt
@@ -0,0 +1,8 @@
+class KotlinClass {
+    fun example() {
+        // Open "Configure keymap...", type "Basic". Look for "Completion -> Basic".
+        // Change the shortcut if it conflicts with your operating system (e.g. keyboard layout switch).
+        // Move the cursor to the end of the next line and invoke this shortcut three times.
+        TestThird
+    }
+}
