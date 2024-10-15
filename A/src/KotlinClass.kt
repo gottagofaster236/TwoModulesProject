98ebae2eb6b2344f13a1bf04548f26a6035aaa50
Lev Leontev <lev.leontev@jetbrains.com>
2024-10-15 10:34:03 -0700

    better comment

--- A/src/KotlinClass.kt
+++ A/src/KotlinClass.kt
@@ -1,6 +1,6 @@
 class KotlinClass {
     fun example() {
-        // Open "Configure keymap...", type "Basic". Look for "Completion -> Basic".
+        // Open "Keymap", "Configure Keymap". Search for "Basic". See the shortcut the one under the "Code Completion" group.
         // Change the shortcut if it conflicts with your operating system (e.g. keyboard layout switch).
         // Move the cursor to the end of the next line and invoke this shortcut three times.
         TestThird
